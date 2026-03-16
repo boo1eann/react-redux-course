3df7fd167a7f0103861452eed869e2e47b6216ce
Damir <damircodenn@gmail.com>
2026-03-16 11:56:50 +0500

    useState

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,18 @@
 import './Button.css';
+import { useState } from 'react';
 
 function Button() {
+  const [text, setText] = useState('Save');
+  console.log('Rerender');
+
   const clicked = () => {
-    console.log('hello!');
+    setText('Close');
+    console.log(text);
   };
 
   return (
     <button onClick={clicked} className="button accent">
-      Save
+      {text}
     </button>
   );
 }
